884ea6bdcae995bcea488d5a1d6ca3b5cc82e745
Guilherme <guilhermet213@gmail.com>
2021-06-27 17:14:44 -0300

    feat: implement nav menu toggle and add header logo

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,15 @@
 const htmlNode = document.documentElement;
 
+function toggleOpenMenu() {
+  const menuElement = document.querySelector('#menu');
+  const menuIconLines = document.querySelectorAll('.navbar__path');
+
+  menuElement.classList.toggle('open');
+  htmlNode.classList.toggle('no-scroll')
+  menuIconLines.forEach(line => line.classList.toggle('open'));
+}
+
+
 function toggleDarkMode() {
   htmlNode.classList.toggle('dark-theme');
   document.querySelector('#light-sun').classList.toggle('hide');

61a19ecf08f039dfde5249baf165995429a5dbc5
Guilherme <guilhermet213@gmail.com>
2021-06-28 19:55:33 -0300

    feat: save dark mode state on local storage

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,25 @@ function toggleOpenMenu() {
   const menuIconLines = document.querySelectorAll('.navbar__path');
 
   menuElement.classList.toggle('open');
-  htmlNode.classList.toggle('no-scroll')
-  menuIconLines.forEach(line => line.classList.toggle('open'));
+  htmlNode.classList.toggle('no-scroll');
+  menuIconLines.forEach((line) => line.classList.toggle('open'));
 }
-
 
 function toggleDarkMode() {
   htmlNode.classList.toggle('dark-theme');
   document.querySelector('#light-sun').classList.toggle('hide');
   document.querySelector('#dark-moon').classList.toggle('hide');
+
+  if (htmlNode.classList.contains('dark-theme')) {
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
   }
+}
+
+if (localStorage.getItem('darkMode') == 'enabled') {
+  htmlNode.classList.toggle('dark-theme');
+  document.querySelector('#light-sun').classList.toggle('hide');
+  document.querySelector('#dark-moon').classList.toggle('hide');
+}
+
